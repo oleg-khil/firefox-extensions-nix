37fa29677e40fa62232f2e1c052f3fc2151b54a3
oleg-khil <155095028+oleg-khil@users.noreply.github.com>
2025-03-14 21:50:50 +0300

    init ci

--- flake.nix
+++ flake.nix
@@ -2,20 +2,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { nixpkgs, ... }:
-  let
-    eachSystem = f: builtins.mapAttrs (system: _: f system) nixpkgs.legacyPackages;
-  in
-  {
-    packages = eachSystem (system:
-      let
-       pkgs = import nixpkgs { inherit system; };
-       buildFirefoxXpiAddon = import ./build_firefox_xpi_addon.nix { inherit pkgs; };
-      in
-      import ./addons.nix {
-        lib = pkgs.lib;
-        inherit buildFirefoxXpiAddon;
-      }
-    );
-  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem = f: builtins.mapAttrs (system: _: f system) nixpkgs.legacyPackages;
+    in
+    {
+      packages = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          buildFirefoxXpiAddon = import ./build_firefox_xpi_addon.nix { inherit pkgs; };
+          addons = import ./addons.nix {
+            lib = pkgs.lib;
+            inherit buildFirefoxXpiAddon;
+          };
+        in
+        addons
+        // {
+          all-addons-combined = pkgs.symlinkJoin {
+            pname = "all-addons-combined";
+            version = "1";
+            paths = builtins.attrValues addons;
+          };
+        }
+      );
+    };
 }
